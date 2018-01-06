a3e150b75c5044b4e7d524f156011b426622512d
Renato Abreu <renatoabreu1196@gmail.com>
2018-01-06 00:20:11 +0000

    added examples to participants

--- RallyJava/src/gui/ParticipantsManager.java
+++ RallyJava/src/gui/ParticipantsManager.java
@@ -73,37 +73,12 @@ public class ParticipantsManager {
 
     ParticipantsManager(MainWindow mainWindow) {
         this.parent = mainWindow;
-        manufacturerCountry.addItem(GermanyQuote.getInstance());
-        manufacturerCountry.addItem(GreatBritainQuote.getInstance());
-        manufacturerCountry.addItem(ItalyQuote.getInstance());
-        manufacturerCountry.addItem(JapanQuote.getInstance());
-        manufacturerCountry.addItem(FranceQuote.getInstance());
-        manufacturerCountry.addItem(PortugalQuote.getInstance());
-        manufacturerCountry.addItem(SouthKoreaQuote.getInstance());
-        manufacturerCountry.addItem(SpainQuote.getInstance());
-
+        initSponsors();
+        initManufacturers();
+        initCars();
         carManufacturer.addItem("None");
-
-        driverNationality.addItem(GermanyQuote.getInstance());
-        driverNationality.addItem(GreatBritainQuote.getInstance());
-        driverNationality.addItem(ItalyQuote.getInstance());
-        driverNationality.addItem(JapanQuote.getInstance());
-        driverNationality.addItem(FranceQuote.getInstance());
-        driverNationality.addItem(PortugalQuote.getInstance());
-        driverNationality.addItem(SouthKoreaQuote.getInstance());
-        driverNationality.addItem(SpainQuote.getInstance());
-        driverGender.addItem(MaleQuote.getInstance());
-        driverGender.addItem(FemaleQuote.getInstance());
-        driverBirth.setText("2017-10-01");
-
-        teamBased.addItem(GermanyQuote.getInstance());
-        teamBased.addItem(GreatBritainQuote.getInstance());
-        teamBased.addItem(ItalyQuote.getInstance());
-        teamBased.addItem(JapanQuote.getInstance());
-        teamBased.addItem(FranceQuote.getInstance());
-        teamBased.addItem(PortugalQuote.getInstance());
-        teamBased.addItem(SouthKoreaQuote.getInstance());
-        teamBased.addItem(SpainQuote.getInstance());
+        initDrivers();
+        initTeams();
 
         addListeners();
     }
@@ -126,7 +101,7 @@ public class ParticipantsManager {
             String manName = (String) teamBacker.getSelectedItem();
             Manufacturer m = findManufacturer(manName);
 
-            if(m == null)
+            if(m == null || teamPicker.getSelectedItem() == null)
                 return;
             Integer teamIndex = teamPicker.getSelectedIndex();
             teams.get(teamIndex).SetBacker(m);
@@ -134,6 +109,8 @@ public class ParticipantsManager {
         });
 
         removeTeamBackerButton.addActionListener(e -> {
+            if(teamPicker.getSelectedItem() == null)
+                return;
             Integer teamIndex = teamPicker.getSelectedIndex();
             teams.get(teamIndex).RemoveBacker();
             setTeamsArea();
@@ -141,11 +118,10 @@ public class ParticipantsManager {
 
         addTeamSponsorButton.addActionListener(e -> {
             String sponsorName = (String) teamSponsors.getSelectedItem();
-            Sponsor s = sponsors.stream()
-                    .filter(obj -> obj.GetName().equals(sponsorName))
-                    .findFirst()
-                    .orElse(null);
+            Sponsor s = findSponsor(sponsorName);
 
+            if(teamPicker.getSelectedItem() == null)
+                return;
             if(s != null) {
                 Integer teamIndex = teamPicker.getSelectedIndex();
                 teams.get(teamIndex).AddSponsor(s);
@@ -155,11 +131,10 @@ public class ParticipantsManager {
 
         removeTeamSponsorButton.addActionListener(e -> {
             String sponsorName = (String) teamSponsors.getSelectedItem();
-            Sponsor s = sponsors.stream()
-                    .filter(obj -> obj.GetName().equals(sponsorName))
-                    .findFirst()
-                    .orElse(null);
+            Sponsor s = findSponsor(sponsorName);
 
+            if(teamPicker.getSelectedItem() == null)
+                return;
             if(s != null) {
                 Integer teamIndex = teamPicker.getSelectedIndex();
                 teams.get(teamIndex).RemoveSponsor(s);
@@ -169,11 +144,10 @@ public class ParticipantsManager {
 
         addTeamCarButton.addActionListener(e -> {
             String carName = (String) teamCars.getSelectedItem();
-            Car c = cars.stream()
-                    .filter(obj -> obj.GetModel().equals(carName))
-                    .findFirst()
-                    .orElse(null);
+            Car c = findCar(carName);
 
+            if(teamPicker.getSelectedItem() == null)
+                return;
             if(c != null) {
                 Integer teamIndex = teamPicker.getSelectedIndex();
                 teams.get(teamIndex).AddCar(c);
@@ -184,11 +158,10 @@ public class ParticipantsManager {
 
         removeTeamCarButton.addActionListener(e -> {
             String carName = (String) teamCars.getSelectedItem();
-            Car c = cars.stream()
-                    .filter(obj -> obj.GetModel().equals(carName))
-                    .findFirst()
-                    .orElse(null);
+            Car c = findCar(carName);
 
+            if(teamPicker.getSelectedItem() == null)
+                return;
             if(c != null) {
                 Integer teamIndex = teamPicker.getSelectedIndex();
                 teams.get(teamIndex).RemoveCar(c);
@@ -199,11 +172,10 @@ public class ParticipantsManager {
 
         addTeamDriverButton.addActionListener(e -> {
             String driverName = (String) teamDrivers.getSelectedItem();
-            Driver d = drivers.stream()
-                    .filter(obj -> obj.GetName().equals(driverName))
-                    .findFirst()
-                    .orElse(null);
+            Driver d = findDriver(driverName);
 
+            if(teamPicker.getSelectedItem() == null)
+                return;
             if(d != null) {
                 Integer teamIndex = teamPicker.getSelectedIndex();
                 teams.get(teamIndex).AddDriver(d);
@@ -214,11 +186,10 @@ public class ParticipantsManager {
 
         removeTeamDriverButton.addActionListener(e -> {
             String driverName = (String) teamDrivers.getSelectedItem();
-            Driver d = drivers.stream()
-                    .filter(obj -> obj.GetName().equals(driverName))
-                    .findFirst()
-                    .orElse(null);
+            Driver d = findDriver(driverName);
 
+            if(teamPicker.getSelectedItem() == null)
+                return;
             if(d != null) {
                 Integer teamIndex = teamPicker.getSelectedIndex();
                 teams.get(teamIndex).RemoveDriver(d);
@@ -241,18 +212,12 @@ public class ParticipantsManager {
         assignCarToDriverButton.addActionListener(e -> {
             String carName = (String) teamAssignCar.getSelectedItem();
             String driverName = (String) teamAssignDriver.getSelectedItem();
-
-            Driver d = drivers.stream()
-                    .filter(obj -> obj.GetName().equals(driverName))
-                    .findFirst()
-                    .orElse(null);
-
-            Car c = cars.stream()
-                    .filter(obj -> obj.GetModel().equals(carName))
-                    .findFirst()
-                    .orElse(null);
+            Driver d = findDriver(driverName);
+            Car c = findCar(carName);
 
             if(c == null || d == null)
+                return;
+            if(teamPicker.getSelectedItem() == null)
                 return;
 
             Integer teamIndex = teamPicker.getSelectedIndex();
@@ -262,13 +227,11 @@ public class ParticipantsManager {
 
         unassignCarFromDriverButton.addActionListener(e -> {
             String driverName = (String) teamAssignDriver.getSelectedItem();
-
-            Driver d = drivers.stream()
-                    .filter(obj -> obj.GetName().equals(driverName))
-                    .findFirst()
-                    .orElse(null);
+            Driver d = findDriver(driverName);
 
             if(d == null)
+                return;
+            if(teamPicker.getSelectedItem() == null)
                 return;
 
             Integer teamIndex = teamPicker.getSelectedIndex();
@@ -335,6 +298,34 @@ public class ParticipantsManager {
         manufacturersTP.setText(sb.toString());
     }
 
+    private Manufacturer findManufacturer(String name) {
+        return manufacturers.stream()
+                .filter(obj -> obj.GetName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    private Sponsor findSponsor(String name) {
+        return sponsors.stream()
+                .filter(obj -> obj.GetName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    private Car findCar(String model) {
+        return cars.stream()
+                .filter(obj -> obj.GetModel().equals(model))
+                .findFirst()
+                .orElse(null);
+    }
+
+    private Driver findDriver(String name) {
+        return drivers.stream()
+                .filter(obj -> obj.GetName().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
+
     public void setVisible() {
         this.pane.setVisible(true);
     }
@@ -347,14 +338,190 @@ public class ParticipantsManager {
         return backButton;
     }
 
-    private Manufacturer findManufacturer(String name) {
-        return manufacturers.stream()
-                .filter(obj -> obj.GetName().equals(name))
-                .findFirst()
-                .orElse(null);
-    }
-
     public ArrayList<Team> getTeams() {
         return teams;
     }
+
+    private void initDrivers() {
+        driverNationality.addItem(GermanyQuote.getInstance());
+        driverNationality.addItem(GreatBritainQuote.getInstance());
+        driverNationality.addItem(ItalyQuote.getInstance());
+        driverNationality.addItem(JapanQuote.getInstance());
+        driverNationality.addItem(FranceQuote.getInstance());
+        driverNationality.addItem(PortugalQuote.getInstance());
+        driverNationality.addItem(SouthKoreaQuote.getInstance());
+        driverNationality.addItem(SpainQuote.getInstance());
+        driverGender.addItem(MaleQuote.getInstance());
+        driverGender.addItem(FemaleQuote.getInstance());
+        driverBirth.setText("2017-10-01");
+
+        drivers.add(new Driver(
+                "Dan",
+                MaleQuote.getInstance(),
+                GreatBritainQuote.getInstance(),
+                DateUtils.MakeDate(1980L, 10L, 5L)));
+        drivers.add(new Driver(
+                "Steven",
+                FemaleQuote.getInstance(),
+                SpainQuote.getInstance(),
+                DateUtils.MakeDate(1987L, 2L, 22L)));
+        drivers.add(new Driver(
+                "Peter",
+                MaleQuote.getInstance(),
+                GermanyQuote.getInstance(),
+                DateUtils.MakeDate(1995L, 5L, 29L)));
+        drivers.add(new Driver(
+                "John",
+                MaleQuote.getInstance(),
+                GreatBritainQuote.getInstance(),
+                DateUtils.MakeDate(1980L, 10L, 5L)));
+        drivers.add(new Driver(
+                "Mark",
+                FemaleQuote.getInstance(),
+                SpainQuote.getInstance(),
+                DateUtils.MakeDate(1987L, 2L, 22L)));
+        drivers.add(new Driver(
+                "Alonso",
+                MaleQuote.getInstance(),
+                GermanyQuote.getInstance(),
+                DateUtils.MakeDate(1995L, 5L, 29L)));
+        drivers.add(new Driver(
+                "Thomas",
+                MaleQuote.getInstance(),
+                GreatBritainQuote.getInstance(),
+                DateUtils.MakeDate(1980L, 10L, 5L)));
+        drivers.add(new Driver(
+                "Smalling",
+                FemaleQuote.getInstance(),
+                SpainQuote.getInstance(),
+                DateUtils.MakeDate(1987L, 2L, 22L)));
+        drivers.add(new Driver(
+                "Marco",
+                MaleQuote.getInstance(),
+                GermanyQuote.getInstance(),
+                DateUtils.MakeDate(1995L, 5L, 29L)));
+
+        setDriversArea();
+
+        teamDrivers.addItem("Dan");
+        teamDrivers.addItem("Steven");
+        teamDrivers.addItem("Peter");
+        teamDrivers.addItem("John");
+        teamDrivers.addItem("Mark");
+        teamDrivers.addItem("Alonso");
+        teamDrivers.addItem("Thomas");
+        teamDrivers.addItem("Smalling");
+        teamDrivers.addItem("Marco");
+    }
+
+    private void initCars() {
+        cars.add(new Car("Hyundai i20",
+                findManufacturer("Hyunday"),
+                370L,
+                1250L));
+
+        cars.add(new Car("Citroën C3",
+                findManufacturer("Citroën"),
+                400L,
+                1300L));
+
+        cars.add(new Car("Toyota Yaris",
+                findManufacturer("Toyota"),
+                480L,
+                1200L));
+
+
+        cars.add(new Car("Citroën C4",
+                findManufacturer("Citroën"),
+                400L,
+                1280));
+
+        cars.add(new Car("Ford Fiesta",
+                null,
+                480L,
+                1200L));
+
+        setCarsArea();
+        teamCars.addItem("Hyundai i20");
+        teamCars.addItem("Citroën C3");
+        teamCars.addItem("Toyota Yaris");
+        teamCars.addItem("Citroën C4");
+        teamCars.addItem("Ford Fiesta");
+    }
+
+    private void initTeams() {
+        teamBased.addItem(GermanyQuote.getInstance());
+        teamBased.addItem(GreatBritainQuote.getInstance());
+        teamBased.addItem(ItalyQuote.getInstance());
+        teamBased.addItem(JapanQuote.getInstance());
+        teamBased.addItem(FranceQuote.getInstance());
+        teamBased.addItem(PortugalQuote.getInstance());
+        teamBased.addItem(SouthKoreaQuote.getInstance());
+        teamBased.addItem(SpainQuote.getInstance());
+
+        teams.add(new Team("CITROËN Total Abu Dhabi WRT", FranceQuote.getInstance()));
+        teams.add(new Team("Hyundai Motorsport", GermanyQuote.getInstance()));
+        teams.add(new Team("M-Sport World Rally Team", GreatBritainQuote.getInstance()));
+        teams.add(new Team("TOYOTA Gazoo Racing WRT", JapanQuote.getInstance()));
+
+        setTeamsArea();
+        teamPicker.addItem("CITROËN Total Abu Dhabi WRT");
+        teamPicker.addItem("Hyundai Motorsport");
+        teamPicker.addItem("M-Sport World Rally Team");
+        teamPicker.addItem("TOYOTA Gazoo Racing WRT");
+    }
+
+    private void initSponsors() {
+        sponsors.add(new Sponsor(
+                "Abu Dhabi",
+                "Official visitor website for Abu Dhabi travel and tourism, offering information on hotels, restaurants, things to do, culture & heritage and events."));
+        sponsors.add(new Sponsor(
+                "Eparco",
+                "Spécialiste assainissement compact, développe et fabrique en exclusivité des solutions pour l'assainissement non collectif depuis 45 ans."));
+        sponsors.add(new Sponsor(
+                "Stilo",
+                "Stilo helmets competition. Top functionality, everything a driver needs must be standard feature: intercom, connections, earmuffs, drinking system."));
+        sponsors.add(new Sponsor(
+                "Michelin",
+                "Michelin, the leading tire company, is dedicated to enhancing its clients’ mobility, sustainably; designing and distributing the most suitable tires, services and solutions for its clients’ needs."));
+
+        setSponsorsArea();
+        teamSponsors.addItem("Abu Dhabi");
+        teamSponsors.addItem("Eparco");
+        teamSponsors.addItem("Stilo");
+        teamSponsors.addItem("Michelin");
+    }
+
+    private void initManufacturers() {
+        manufacturerCountry.addItem(GermanyQuote.getInstance());
+        manufacturerCountry.addItem(GreatBritainQuote.getInstance());
+        manufacturerCountry.addItem(ItalyQuote.getInstance());
+        manufacturerCountry.addItem(JapanQuote.getInstance());
+        manufacturerCountry.addItem(FranceQuote.getInstance());
+        manufacturerCountry.addItem(PortugalQuote.getInstance());
+        manufacturerCountry.addItem(SouthKoreaQuote.getInstance());
+        manufacturerCountry.addItem(SpainQuote.getInstance());
+
+        manufacturers.add(new Manufacturer(
+                "Citroën",
+                "Citroën is one of the world's leading mainstream car manufacturers.",
+                FranceQuote.getInstance()));
+        manufacturers.add(new Manufacturer(
+                "Toyota",
+                "Toyota is one of the world’s best-known and most successful businesses, building cars and trucks in 26 countries for sale in more than 160 markets around the globe.",
+                JapanQuote.getInstance()));
+        manufacturers.add(new Manufacturer(
+                "Hyunday",
+                "Hyundai Motor Company leads the Hyundai Motor Group, a wide-reaching business capable of processing resources from molten iron to finished cars.",
+                SouthKoreaQuote.getInstance()));
+
+        setManufacturersArea();
+        carManufacturer.addItem("Citroën");
+        teamBacker.addItem("Citroën");
+        carManufacturer.addItem("Toyota");
+        teamBacker.addItem("Toyota");
+        carManufacturer.addItem("Hyunday");
+        teamBacker.addItem("Hyunday");
+    }
+
 }
