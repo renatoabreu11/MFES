c834988daa0f10af515536589c3c82b863be8a7f
Renato Abreu <renatoabreu1196@gmail.com>
2018-01-06 00:41:59 +0000

    championship data

--- RallyJava/src/gui/ChampionshipManager.java
+++ RallyJava/src/gui/ChampionshipManager.java
@@ -54,7 +54,7 @@ public class ChampionshipManager {
     private JButton addPartnerToChampionshipButton;
 
     // Team - Championship
-    private JComboBox championshipTeamPicker;
+    private JComboBox<String> championshipTeamPicker;
     private JButton addTeamToChampionshipButton;
     private JButton removeTeamFromChampionshipButton;
 
@@ -70,14 +70,59 @@ public class ChampionshipManager {
 
     ChampionshipManager(MainWindow mainWindow) {
         this.parent = mainWindow;
-        teams = parent.GetParticipants();
-        stageDate.setText("2017-10-01");
-        rallyStartDate.setText("2017-10-01");
-        rallyEndDate.setText("2017-10-01");
+        initTeams();
+        initPartners();
+        initStages();
+        initRallies();
+        initChampionships();
+        addListeners();
+    }
+
+    private void initChampionships() {
+        championships.add(new Championship("WRC", DateUtils.MakeDate(2017L, 10L, 1L), DateUtils.MakeDate(2017L, 12L, 18L)));
+        setChampionshipsArea();
+        championshipPicker.addItem("WRC");
         championshipStartDate.setText("2017-10-01");
-        championshipEndDate.setText("2017-10-01");
+        championshipEndDate.setText("2017-12-18");
+    }
+
+    private void initStages() {
+        stageDate.setText("2017-10-03");
         stageType.addItem(SpecialQuote.getInstance());
         stageType.addItem(TransportQuote.getInstance());
+
+        stages.add(new Stage(
+                1L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 10L, 5L), 10L));
+        stages.add(new Stage(
+                2L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 10L, 7L), 50L));
+        stages.add(new Stage(
+                3L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 10L, 8L), 3L));
+        stages.add(new Stage(
+                4L, TransportQuote.getInstance(), DateUtils.MakeDate(2017L, 10L, 9L), 100L));
+        stages.add(new Stage(
+                5L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 10L, 11L), 10L));
+        stages.add(new Stage(
+                6L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 11L, 5L), 50L));
+        stages.add(new Stage(
+                7L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 11L, 6L), 3L));
+        stages.add(new Stage(
+                8L, TransportQuote.getInstance(), DateUtils.MakeDate(2017L, 11L, 7L), 100L));
+        stages.add(new Stage(
+                9L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 11L, 9L), 10L));
+        stages.add(new Stage(
+                10L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 12L, 14L), 50L));
+        stages.add(new Stage(
+                11L, SpecialQuote.getInstance(), DateUtils.MakeDate(2017L, 12L, 15L), 3L));
+        stages.add(new Stage(
+                12L, TransportQuote.getInstance(), DateUtils.MakeDate(2017L, 12L, 16L), 100L));
+
+        setStagesArea();
+        stages.forEach(s -> rallyStagePicker.addItem(s.GetId()));
+    }
+
+    private void initRallies() {
+        rallyStartDate.setText("2017-10-03");
+        rallyEndDate.setText("2017-10-08");
         rallySurface.addItem(AsphaltQuote.getInstance());
         rallySurface.addItem(ForestQuote.getInstance());
         rallySurface.addItem(GravelQuote.getInstance());
@@ -92,7 +137,55 @@ public class ChampionshipManager {
         rallyCountry.addItem(PortugalQuote.getInstance());
         rallyCountry.addItem(SouthKoreaQuote.getInstance());
         rallyCountry.addItem(SpainQuote.getInstance());
-        addListeners();
+
+        rallies.add(new Rally("Rallye Monte-Carlo",
+                "The Monte Carlo Rally or Rallye Monte Carlo is a rallying event organised each year by the Automobile Club de Monaco.",
+                FranceQuote.getInstance(),
+                AsphaltQuote.getInstance(),
+                DateUtils.MakeDate(2017L, 10L, 4L),
+                DateUtils.MakeDate(2017L, 10L, 12L)));
+        rallies.add(new Rally("Vodafone Rally de Portugal",
+                "O Rali de Portugal é a maior prova de desporto motorizado que se realiza em Portugal anualmente.",
+                PortugalQuote.getInstance(),
+                GravelQuote.getInstance(),
+                DateUtils.MakeDate(2017L, 11L, 4L),
+                DateUtils.MakeDate(2017L, 11L, 10L)));
+        rallies.add(new Rally("Rally Italia Sardegna",
+                "Held on the beautiful island of Sardinia, Rally Italy – or Rally Italia Sardegna to give it its full name – is a notoriously tricky event.",
+                ItalyQuote.getInstance(),
+                MountainQuote.getInstance(),
+                DateUtils.MakeDate(2017L, 12L, 13L),
+                DateUtils.MakeDate(2017L, 12L, 17L)));
+
+        setRalliesArea();
+        rallyPicker.addItem("Rallye Monte-Carlo");
+        rallyPicker.addItem("Vodafone Rally de Portugal");
+        rallyPicker.addItem("Rally Italia Sardegna");
+        championshipRallyPicker.addItem("Rallye Monte-Carlo");
+        championshipRallyPicker.addItem("Vodafone Rally de Portugal");
+        championshipRallyPicker.addItem("Rally Italia Sardegna");
+    }
+
+    private void initPartners() {
+        partners.add(new Partner(
+                "Michelin",
+                "Michelin, the leading tire company, is dedicated to enhancing its clients’ mobility, sustainably; designing and distributing the most suitable tires, services and solutions for its clients’ needs."));
+        partners.add(new Partner(
+                "Certina",
+                "Precision, reliability, innovation and dynamism. These are the values that have always linked Certina to sport."));
+        partners.add(new Partner(
+                "OneBet",
+                "OneBet is the official sports betting partner of the FIA World Rally Championship, providing spectacular opportunities to boost the rush and experience of WRC."));
+
+        setPartnersArea();
+        championshipPartnerPicker.addItem("Michelin");
+        championshipPartnerPicker.addItem("Certina");
+        championshipPartnerPicker.addItem("OneBet");
+    }
+
+    private void initTeams() {
+        teams = parent.GetParticipants();
+        teams.forEach(t -> championshipTeamPicker.addItem(t.GetName()));
     }
 
     private void addListeners() {
@@ -139,7 +232,7 @@ public class ChampionshipManager {
             int stageId = (int) rallyStagePicker.getSelectedItem();
             Stage s = findStage(stageId);
 
-            if(s != null) {
+            if(s != null && rallyPicker.getSelectedItem() != null) {
                 Integer rallyIndex = rallyPicker.getSelectedIndex();
                 rallies.get(rallyIndex).AddStage(s);
                 setRalliesArea();
@@ -149,13 +242,15 @@ public class ChampionshipManager {
             String partnerName = (String) championshipPartnerPicker.getSelectedItem();
             Partner p = findPartner(partnerName);
 
-            if(p != null) {
+            if(p != null && championshipPicker.getSelectedItem() != null) {
                 Integer champIndex = championshipPicker.getSelectedIndex();
                 championships.get(champIndex).AddPartner(p);
                 setChampionshipsArea();
             }
         });
         removePartnerFromChampionshipButton.addActionListener(e -> {
+            if(championshipPicker.getSelectedItem() == null)
+                return;
             String partnerName = (String) championshipPartnerPicker.getSelectedItem();
             Partner p = findPartner(partnerName);
 
@@ -166,6 +261,8 @@ public class ChampionshipManager {
             }
         });
         addRallyToChampionshipButton.addActionListener(e -> {
+            if(championshipPicker.getSelectedItem() == null)
+                return;
             String rallyName = (String) championshipRallyPicker.getSelectedItem();
             Rally r = findRally(rallyName);
 
@@ -176,6 +273,8 @@ public class ChampionshipManager {
             }
         });
         addTeamToChampionshipButton.addActionListener(e -> {
+            if(championshipPicker.getSelectedItem() == null)
+                return;
             String teamName = (String) championshipTeamPicker.getSelectedItem();
             Team t = findTeam(teamName);
 
@@ -186,6 +285,8 @@ public class ChampionshipManager {
             }
         });
         removeTeamFromChampionshipButton.addActionListener(e -> {
+            if(championshipPicker.getSelectedItem() == null)
+                return;
             String teamName = (String) championshipTeamPicker.getSelectedItem();
             Team t = findTeam(teamName);
 
